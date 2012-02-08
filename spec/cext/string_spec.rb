1dc2426100f2ec25c45c0387cfa36692a188ea80
Minwoo Lee <ermaker@gmail.com>
2012-02-08 15:54:08 +0900

    Refactor

--- spec/cext/string_spec.rb
+++ spec/cext/string_spec.rb
@@ -4,17 +4,13 @@ require 'spec_helper'
 require 'cext/string'
 
 describe String do
-  context '#self?' do
-    it 'works' do
-      '회원님'.should be_self
-      '이민우'.should_not be_self
-    end
+  it '#self? works' do
+    '회원님'.should be_self
+    '이민우'.should_not be_self
   end
 
-  context '#image?' do
-    it 'works' do
-      '<사진>'.should be_image
-      'ㅋ'.should_not be_image
-    end
+  it '#image? works' do
+    '<사진>'.should be_image
+    'ㅋ'.should_not be_image
   end
 end
